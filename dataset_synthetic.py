055fefeb6c477eb3b6061abe0838e2343376f0c1
Kehui Yao <kehuiyao@Kehuis-MacBook-Pro-2.local>
2023-06-01 23:39:18 -0700

    add data generation using space-time basis function

--- dataset_synthetic.py
+++ dataset_synthetic.py
@@ -144,12 +144,160 @@ def generate_ST_data_with_separable_covariance(K, L, B, linear_additive=None, no
 
     return y_standardized, y_mean, y_std, spatial_cov, C_spatio_temporal, X
 
+def generate_ST_data_with_space_time_basis_functions(K, L, B, linear_additive=None, non_linear_additive=None, seed=42):
+    """
+    Generate spatio-temporal data with space-time basis functions,
+    y(s,t) = f(s,t) + eta(s, t) + epsilon(s, t),
+    where f(s,t) = linear_additive + non_linear_additive,
+    eta(s,t) is a sum of space-time basis functions with random coefficients.
+
+    :param K:
+    :param L:
+    :param B:
+    :param linear_additive:
+    :param non_linear_additive:
+    :param seed:
+    :return:
+    """
+
+
+    # define random seed generator
+    rng = np.random.RandomState(seed=seed)
+
+    # Define the spatio-temporal domain
+    x = np.linspace(0, 1, K)
+    t = np.linspace(0, 1, L)
+    ############################### Generate f(s,t) ##########################################
+    if linear_additive is not None:
+        # overall mean
+        X0 = np.ones([B, K, L])
+        # linear in time
+        X1 = np.zeros([B, K, L])
+        for i in range(L):
+            X1[:, :, i] = t[i] * 20
+        # linear in space
+        X2 = np.zeros([B, K, L])
+        for i in range(K):
+            X2[:, i, :] = x[i] * 10
+
+        # basis functions (B, K, L, 3)
+        X = np.stack([X0, X1, X2], axis=3)
+
+        # Generate random coefficients
+        beta = rng.normal(0, 1, size=3)  # 3
+
+        # Generate f(s,t) = X(s,t)*beta(s,t)
+        f = np.einsum('bklj, j->bkl', X, beta)  # (B, K, L)
+    elif non_linear_additive is not None:
+        f = np.zeros([B, K, L])
+    else:
+        f = np.zeros([B, K, L])
+
+    ############################### Generate eta(s,t) ##########################################
+    num_space_basis = 5
+    num_time_basis = 5
+    num_space_time_basis = num_time_basis + num_space_basis
+    space_basis_func_list = []
+    time_basis_func_list = []
+
+    # Generate space basis functions
+    def space_basis_func_generator(i):
+        def space_basis_func(x):
+            if np.abs(x-i) < 0.2:
+                return np.power(1 - np.power(np.abs(x-i) / 0.2, 2), 2)
+            else:
+                return 0
+        return space_basis_func
+
+    def time_basis_func_generator(i):
+        def time_basis_func(t):
+            if np.abs(t-i) < 0.2:
+                return np.power(1 - np.power(np.abs(t-i) / 0.2, 2), 2)
+            else:
+                return 0
+        return time_basis_func
+
+
+    for i in range(num_space_basis):
+        space_basis_func_list.append(space_basis_func_generator(1 / num_space_basis * i))
+
+    # Generate time basis functions
+    for i in range(num_time_basis):
+        time_basis_func_list.append(time_basis_func_generator(1 / num_time_basis * i))
+
+    space_time_basis_func_list = space_basis_func_list + time_basis_func_list
+
+    # create a array of (num_space_basis, K, L)
+    space_basis_values = np.zeros([num_space_basis, K, L])
+    for l in range(num_space_basis):
+        for i in range(K):
+            for j in range(L):
+                space_basis_values[l, i, j] = space_basis_func_list[l](x[i])
+
+    # create a array of (num_time_basis, K, L)
+    time_basis_values = np.zeros([num_time_basis, K, L])
+    for l in range(num_time_basis):
+        for i in range(K):
+            for j in range(L):
+                time_basis_values[l, i, j] = time_basis_func_list[l](t[j])
+
+    # concatenate space and time basis values to get a array of (num_space_time_basis, K, L)
+    space_time_basis_values = np.concatenate([space_basis_values, time_basis_values], axis=0)
+
+    # Generate random coefficients with size (B, num_space_time_basis)
+    eta_coefficients = rng.normal(0, 1, size=(B, num_space_time_basis))
+
+    # Generate eta(s,t) = sum_{l=1}^{num_space_time_basis} eta_coefficients[l] * space_time_basis_values[l]
+    eta = np.einsum('bn, nkl->bkl', eta_coefficients, space_time_basis_values)  # (B, K, L)
+
+
+    ############################### Generate epsilon(s,t) ##########################################
+    # Generate independent standard normal random variables
+    epsilon = rng.normal(0, 1, size=(B, K, L))  # (B, K, L)
+
+    ############################### Generate y(s,t) ##########################################
+    # y = f + eta + epsilon
+    y = f + eta + epsilon
+
+    # Plot the generated data
+    plt.imshow(y[0, :, :], cmap='jet')
+    plt.show()
+
+    # standardize the data, record the mean and std
+    y_mean = np.mean(y)
+    y_std = np.std(y)
+    y_standardized = (y - y_mean) / y_std
+
+    # spatio-temporal covariance matrix of y
+    C_spatio_temporal = None
+    spatial_cov = None
+
+    # covariates time
+    X_time = np.zeros([B, K, L])
+    for i in range(L):
+        X_time[:, :, i] = t[i] * 20
+
+    # covariates space
+    X_space = np.zeros([B, K, L])
+    for i in range(K):
+        X_space[:, i, :] = x[i] * 10
+
+    X = np.stack([X_time, X_space], axis=3)  # (B, K, L, 2)
+
+    return y_standardized, y_mean, y_std, spatial_cov, C_spatio_temporal, X
+
+
+
+
+
 
 if __name__ == "__main__":
     K = 36
     T = 36
     B = 1
-    output, *_ = generate_ST_data_with_separable_covariance(K, T, B, seed=42)
+    # output, *_ = generate_ST_data_with_separable_covariance(K, T, B, seed=42)
     # output, *_ = generate_ST_data_with_separable_covariance(K, T, B, seed=42, linear_additive=True)
+    output, *_ = generate_ST_data_with_space_time_basis_functions(K, T, B, linear_additive=None, non_linear_additive=None, seed=42)
     print(output.shape)
+
 
